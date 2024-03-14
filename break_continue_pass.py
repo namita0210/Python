51871bc6674d08e0dff9c9cdf226335a4c08a5a7
Namita <46557775+namita0210@users.noreply.github.com>
2024-03-14 10:10:28 +0000

    continue keyword in python

---
+++ break_continue_pass.py
@@ -0,0 +1,14 @@
+for i in range (1, 30):
+    if i%3==0:
+        print("Multiple of three")
+        continue
+    print(i)
+
+i = 0
+while ( i <= 20):
+    i+=1
+    if i%2==0:
+        print("multiple of 2")
+        continue
+    print(i)
+    
