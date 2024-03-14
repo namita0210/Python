9a4f8ec0c732f3fbbf0fbf899b0b9e43cbce34ae
Namita <46557775+namita0210@users.noreply.github.com>
2024-03-14 14:55:03 +0000

    matrix

--- numpyLearning.py
+++ numpyLearning.py
@@ -4,5 +4,17 @@ from numpy import *
 # print(arr)
 # print(type(arr))
 
-arr = arange(2,20,2)
+# arr = arange(2,20,2)
+# print(arr)
+
+# arr = array([[1,2,3],[54,3,2]])
+# arr=arr.flatten()
+# print(arr)
+# print(arr.ndim)
+# print(arr.shape)
+
+arr = array([1,2,3,4,5,6,7,8,9,10,11,12])
 print(arr)
+arr = arr.reshape(2,2,3)
+print(arr)
+print(arr.ndim)
